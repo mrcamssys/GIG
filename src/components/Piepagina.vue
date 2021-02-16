57f2b0942a63741a768955940596f327328a0a2b
Carlos Arturo Moreno <39220043+mrcamssys@users.noreply.github.com>
2021-02-15 23:49:30 -0500

    Add files via upload

--- src/components/Piepagina.vue
+++ src/components/Piepagina.vue
@@ -1,69 +1,13 @@
 <template>
   <mdb-footer class="page-footer font-small pt-4 mt-4 ">
-    <hr />
-    <div class="stylish">
-      <div class="divfondo">
-      
-      <mdb-container class="text-left">
+      <mdb-container class="text-left" style="">
+        <div class="iconoscontenido"></div>
         <mdb-row>
-          <mdb-col md="6">
-            <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">
-              Nuestra Institucion
-            </h5>
-            <hr>
-            <p>
-              Hacer parte de la comunidad educativa Guatiquia, implica disfrutar
-              y poner en práctica los derechos y deberes que buscan el
-              fortalecimiento de la autoestima y la autorregulación, basados en
-              el respeto, la autonomía, la sana convivencia y el bienestar
-              individual y colectivo; para que nuestro niños, niñas y jóvenes
-              reflejen desde la cotidianidad su identidad con la institución
-              educativa, en donde el eje central es el ser humano integral como
-              agente de cambio y transformador de su entorno.
-            </p>
-
-            
-          </mdb-col>
-          <hr class="clearfix w-100 d-md-none" />
-          <mdb-col md="3">
-            <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">
-              Paginas Auxiliares
-            </h5> 
-            <hr>
-
+          <mdb-col md="12">
             <div>
                <a v-for="(imgs, id) in imgiconos" :key="id"  @href="imgrutas[id]" target="_blank">
                 <b-img class="iconos"  rounded="0" :src="imgs" ></b-img>
                </a>
-            </div>
-
-          </mdb-col>
-          <hr class="clearfix w-100 d-md-none" />
-          <mdb-col md="3" center>
-            <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">
-              Informacion
-            </h5>
-            <hr>
-            <p>
-              <b>Representante legal:</b><br>
-              Rectora: Lic. Rubiela Angel Galvis.<br>
-              <hr>
-              <b>Comunicacion Secretaria:</b><br>
-              Email: <a href="soporteadministrativo@gimnasioguatiquia.edu.co">soporteadministrativo@gimnasioguatiquia.edu.co</a><br>
-              Celular: +57 319 209 86 59
-              <br>
-            </p>
-            <hr>
-            <!-- <b-img class="loder" src="logos/loder.png" alt="GIG Logo"></b-img> -->
-            <p>
-              <b-icon icon="geo-alt-fill"></b-icon>
-              Cll 59A Sur N° 45d-02/08<br />
-              <b-icon icon="telephone-outbound-fill"></b-icon>
-              Telefono: 715 18 27<br>
-              <b-icon icon="clock"></b-icon>
-              Lunes a Viernes 6:30AM a 2:00PM<br>
-            </p>
-            <div>
                <a href="https://www.educacionbogota.edu.co/portal_institucional/servicio-ciudadania2" target="_blank">
                 <b-img height="80px"  rounded="0" src="https://www.culturarecreacionydeporte.gov.co/sites/default/files/adjuntos_paginas_2014/logo_bogota_png_byn_rojo-01.png" ></b-img>
                </a>
@@ -72,7 +16,44 @@
         </mdb-row>
       </mdb-container>
       
-      
+    <div class="stylish">
+      <div class="divfondo">
+      <mdb-container class="text-left">
+        <mdb-row>
+          <mdb-col md="4">
+            <p><hr>
+              <b-icon icon="geo-alt-fill"></b-icon>
+              Cll 59A Sur N° 45d-02/08<br />
+              <b-icon icon="telephone-outbound-fill"></b-icon>
+              Telefono: 715 18 27<br>
+              <b-icon icon="clock"></b-icon>
+              Lunes a Viernes 6:30AM a 2:00PM<br>
+            </p>
+          </mdb-col>
+          <mdb-col md="4">
+            <p>
+              <hr>
+              <b>Comunicacion Secretaria:</b><br>
+              Email: <div style="overflow-x: scroll;"><a href="smb://soporteadministrativo@gimnasioguatiquia.edu.co">soporteadministrativo@gimnasioguatiquia.edu.co</a></div><br>
+              Celular: +57 319 209 86 59
+              <br>
+            </p>
+            
+          </mdb-col>
+          <mdb-col md="4"><br>
+            <p>
+              <div style="display:inline; text-align: left;">
+              <img height="60px" style="filter: grayscale(100%); display:inline; text-align: left; padding: 4px;" src="logos/loder.png" />
+              <p style="text-align: left; display:inline;"> 
+              <b>Representante legal:</b><br>
+              Rectora: Lic. Rubiela Angel Galvis.<br></p>
+
+              </div>
+            </p>
+          </mdb-col>
+        </mdb-row>
+      </mdb-container>
+
       <hr />
       <div class="footer-copyright text-center py-3 data1">
         <mdb-container fluid>
@@ -133,21 +114,63 @@ export default {
 
 .iconos{
   padding: 10px;
-  height: 100px;
+  height: 70px;
   filter: grayscale(80%);
 }
 .iconos:hover{
   padding: 10px;
-  height: 120px;
+  height: 80px;
   filter: grayscale(0%);
+}
+
+.iconoscontenido{
+  position:absolute;
+  left: 4%;
+  z-index: -5; 
+  width:650px; 
+  border-right: 60px solid transparent; 
+  border-left: 60px solid transparent; 
+  border-bottom: 100px solid #fff;
 }
 
 @media (max-width: 990px), handheld and (orientation: landscape){  
   .divfondo{
     background-image: url('https://i.pinimg.com/originals/ce/10/5f/ce105fda85be60e90b3fec3a1a43b799.jpg') !important;
     background-repeat: no-repeat;
-    background-size: auto 100%;
+    background-size: 100% 100%;
   }
 }
 
+
+@media (max-width: 770px) {  
+  .iconoscontenido{
+    left: 0%;
+    z-index: -5; 
+    width:100%;
+    height: 100%;
+    background-color: #fff; 
+  }
+  
+}
+
 </style>
+
+
+          <!-- <mdb-col md="6">
+            <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">
+              Nuestra Institucion
+            </h5>
+            <hr>
+            <p>
+              Hacer parte de la comunidad educativa Guatiquia, implica disfrutar
+              y poner en práctica los derechos y deberes que buscan el
+              fortalecimiento de la autoestima y la autorregulación, basados en
+              el respeto, la autonomía, la sana convivencia y el bienestar
+              individual y colectivo; para que nuestro niños, niñas y jóvenes
+              reflejen desde la cotidianidad su identidad con la institución
+              educativa, en donde el eje central es el ser humano integral como
+              agente de cambio y transformador de su entorno.
+            </p>
+
+            
+          </mdb-col> -->
