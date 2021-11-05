c3a9892e191878daa5add8ffde7ce934ba44da70
Carlos Arturo Moreno <39220043+mrcamssys@users.noreply.github.com>
2021-11-04 19:02:26 -0500

    Update colapsever.vue

--- src/components/colapsever.vue
+++ src/components/colapsever.vue
@@ -3,15 +3,17 @@
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab" end>
         <b-button block v-b-toggle.accordion-1 variant="info">
-            <b-icon icon="inbox-fill"></b-icon> Comunicados Secretaria Escolar</b-button>
+            <b-icon icon="inbox-fill"></b-icon> Admisiones </b-button>
       </b-card-header>
       <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
         <b-card-body>
-          <b-card-text>{{ text }}</b-card-text>
+          <b-card-text>
+            
+          </b-card-text>
         </b-card-body>
       </b-collapse>
     </b-card>
-
+<!--
     <b-card no-body class="mb-1">
       <b-card-header header-tag="header" class="p-1" role="tab" end>
         <b-button block v-b-toggle.accordion-2 variant="info"><b-icon icon="inbox-fill"></b-icon> Comunicados Academicos</b-button>
@@ -33,6 +35,7 @@
         </b-card-body>
       </b-collapse>
     </b-card>
+    -->
   </div>
 </template>
 
