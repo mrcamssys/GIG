76f77627cb60bf3b50d160eb144afa3e3dcdc55c
Carlos Arturo Moreno <39220043+mrcamssys@users.noreply.github.com>
2021-11-04 20:23:09 -0500

    Update Imgfondo.vue

--- src/components/Imgfondo.vue
+++ src/components/Imgfondo.vue
@@ -9,9 +9,9 @@
         img-width="100%"
         img-height="100%"
       >
-        <b-carousel-slide img-src="/fondos/base1.jpeg"></b-carousel-slide>
-        <b-carousel-slide img-src="/fondos/base2.jpeg"></b-carousel-slide>
-        <b-carousel-slide img-src="/fondos/fondo.jpeg"></b-carousel-slide>
+        <b-carousel-slide img-src="/fondos/base1.jpg"></b-carousel-slide>
+        <b-carousel-slide img-src="/fondos/base2.jpg"></b-carousel-slide>
+        <b-carousel-slide img-src="/fondos/fondo.jpg"></b-carousel-slide>
       </b-carousel>
     </div>
   </div>
