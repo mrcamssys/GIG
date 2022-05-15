db97b2b66deb7aaaa4544c66c116e4ddbd44d632
Carlos Arturo Moreno Susatama <mrcamssys@gmail.com>
2022-05-15 17:24:29 -0500

    fix: reparacion de vista plan de accion y construccion y modificacion del modulo de movilidad y convocatorias movilidad

--- src/components/informacion_pincipal.vue
+++ src/components/informacion_pincipal.vue
@@ -1,25 +1,31 @@
 <template>
-  <div class="contenedorx textcolor">
-    <div class="divisores pl-3">
-        <h3><span class="icon-play" aria-hidden="true"></span> Videos Institucionales</h3>
-    </div>
-    <b-card-text class="p-2">
-    <!-- <iframe width="100%"  src="https://www.youtube.com/list=UUA-uViVcw5eRX70yLo1EgUw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-        <iframe src="https://www.youtube.com/embed/videoseries?list=UUA-uViVcw5eRX70yLo1EgUw" width="430px" height="240px" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
-        El colegio Gimnasio Integral Guatiquia ofrece a la comunidad una
-        educación solida basada en principios y valores éticos, morales
-        y culturales.
-    </b-card-text>
-    <div class="divisores pl-3">
-        <h3><span class="icon-info" aria-hidden="true"></span> Información institucional</h3>
-    </div>
-    <Colapsever class="m-2" />
+   <div>
+       <div class="divisores2 pl-3" v-show="!dataShow" v-on:click="dataShow=true">
+            <h3><span class="icon-arrow-down2" aria-hidden="true" ></span>Mostrar Información</h3>
+        </div>
+       <div class="contenedorx textcolor" v-show="dataShow" v-on:click="dataShow=false">
+            <div class="divisores pl-3" >
+                <h3><span class="icon-play" aria-hidden="true"></span> Videos Institucionales</h3>
+            </div>
+            <b-card-text class="p-2">
+            <!-- <iframe width="100%"  src="https://www.youtube.com/list=UUA-uViVcw5eRX70yLo1EgUw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
+                <iframe src="https://www.youtube.com/embed/videoseries?list=UUA-uViVcw5eRX70yLo1EgUw" width="430px" height="240px" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                El colegio Gimnasio Integral Guatiquia ofrece a la comunidad una
+                educación solida basada en principios y valores éticos, morales
+                y culturales.
+            </b-card-text>
+            <div class="divisores pl-3">
+                <h3><span class="icon-info" aria-hidden="true"></span> Información institucional</h3>
+            </div>
+            <Colapsever class="m-2" />
 
-    <div class="divisores pl-3">
-        <h3><span class="icon-newspaper" aria-hidden="true"></span> Noticias del Día</h3>
-    </div>
-    <spam class="m-3">Error conectando contenido de bing news...</spam>
-  </div>
+            <div class="divisores pl-3">
+                <h3><span class="icon-newspaper" aria-hidden="true"></span> Noticias del Día</h3>
+            </div>
+            <spam class="m-3">Error conectando contenido de bing news...</spam>
+        </div>
+   </div>
+  
 </template>
 
 <script>
@@ -27,6 +33,11 @@ import Colapsever from '../components/colapsever.vue';
 export default {
    components: {
     Colapsever
+  },
+  data(){
+    return{
+      dataShow:true,
+    }
   },
 }
 </script>
@@ -51,5 +62,12 @@ export default {
     left: 0;
     background-color: chartreuse;
     border-radius: 16px 16px 0px 0px;
+}
+.divisores2{
+    color: rgb(194, 189, 189);
+    position:relative;
+    left: 0;
+    background-color: rgb(55, 63, 83);
+    border-radius: 16px 16px 16px 16px;
 }
 </style>
