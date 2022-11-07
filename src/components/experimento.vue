9f226b67272dae10d81420c27e2261f70b0454c9
Carlos Arturo Moreno Susatama <mrcamssys@gmail.com>
2022-11-06 22:51:26 -0500

    feat instalacion vue 3

---
+++ src/components/experimento.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+// mi rpimer componente en api 
+defineProps(
+    {
+        title: String,
+        id: Number,
+        body: {
+            type: String,
+            default: "Sin texto encotrado.",
+        }
+    }
+);
+</script>
+
+<template>
+    <div class="container-fluid mt-0">
+    <div class="card-body">
+      <h5 class="card-title">{{id}} - {{title}}</h5>
+      <p>{{body}}</p>
+    </div>
+  </div>
+</template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,64 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+import {ref} from "vue"
+
+const name="dinamico";
+let styles="color: yellow;"
+
+const contador=ref(0)
+
+const decremento = () => {
+  contador.value--;
+  // cambioColor();
+}
+
+const aumento = () => {
+  contador.value++;
+  // cambioColor()
+}
+
+const cambioColor=()=>{
+  console.warn(contador)
+  console.warn(contador.value > 0)
+  if(contador.value >= 0) return "color: green;"
+  else return "color: red;"
+}
+
+const colorbuton=()=>{
+  console.warn(contador)
+  console.warn(contador.value > 0)
+  if(contador.value >= 0) return "btn-secondary"
+  else return "btn-primary"
+}
+
+
+</script>
+
+
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <!-- <header>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+  </header> -->
+  
+  <div class="container text-center mt-0">
+    <h1>sistema: {{name}}</h1>
+    <h2 :style="cambioColor()">cambio de color: {{contador}}</h2>
+    <button type="button" @click="decremento" :class="'btn '+colorbuton()+' m-1'"> Decremento -- </button>
+    <button type="button" @click="aumento" class="btn btn-secondary m-1" > Incremento ++ </button>
   </div>
+
+  <!-- <RouterView /> -->
 </template>
 
-<style>
+<style scoped>
+
+
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+ 
 }
 </style>
